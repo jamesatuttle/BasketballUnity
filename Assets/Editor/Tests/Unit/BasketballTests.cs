d423ccc20a9f79624e318ae5471dd5c702dd6c59
James Tuttle <jamesatuttle95@gmail.com>
2017-02-05 21:34:23 +0000

    Add basketball unit test file

--- Assets/Editor/Tests/Unit/BasketballTests.cs
+++ Assets/Editor/Tests/Unit/BasketballTests.cs
@@ -1,6 +1,14 @@
 ﻿using NUnit.Framework;
 
+[TestFixture]
+
 public class BasketballTests {
 
-	
+    [Test]
+	public void ResetBall_CorrectPosition()
+    {
+        //tests to make sure that the ball starts in the correct location
+        var basketball = new Basketball();
+
+    }
 }
