5627fa6dd55c072506c9711815233e61d1a26b46
James Tuttle <jamesatuttle95@gmail.com>
2017-02-06 02:46:42 +0000

    Added Basketball tests

--- Assets/Editor/Tests/Unit/BasketballTests.cs
+++ Assets/Editor/Tests/Unit/BasketballTests.cs
@@ -1,14 +1,24 @@
 ﻿using NUnit.Framework;
+using UnityEngine;
 
 [TestFixture]
 
 public class BasketballTests {
 
+    GameObject basketball = GameObject.Find("Basketball");
+
     [Test]
 	public void ResetBall_CorrectPosition()
     {
         //tests to make sure that the ball starts in the correct location
-        //var basketball = new Basketball();
+        Basketball.ResetBall();
+        Assert.AreEqual(new Vector3(0.02f, 5.08f, 9.96f), basketball.transform.position);
+    }
 
+    [Test]
+    public void ResetBall_Gravity_Off()
+    {
+        Basketball.ResetBall();
+        Assert.AreEqual(false, basketball.GetComponent<Rigidbody>().useGravity);
     }
 }
