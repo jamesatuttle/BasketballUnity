8be1d3e28d3a109c538c89f7f39e9e5d8132e595
James Tuttle <jamesatuttle95@gmail.com>
2017-02-06 02:46:30 +0000

    Remove old tests from GamePlayTests

--- Assets/Editor/Tests/Unit/GamePlayTests.cs
+++ Assets/Editor/Tests/Unit/GamePlayTests.cs
@@ -5,19 +5,5 @@ using AssemblyCSharp;
 
 public class GamePlayTests {
 
-	[Test]
-	public void isGamePlayable_InitialValue()
-	{
-		Scoreboard.availableBalls = 3;
-		//Assert.AreEqual(GamePlay.isGamePlayable, true);
-		Assert.AreEqual(true, GamePlay.isGamePlayable);
-	}
-
-	[Test]
-	public void isGamePlayable_NoMoreBalls()
-	{
-		Scoreboard.availableBalls = 0;
-		Assert.AreEqual(0, Scoreboard.availableBalls);
-		//Assert.AreEqual(false, GamePlay.isGamePlayable);
-	}
+	
 }
