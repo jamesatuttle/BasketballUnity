e6d85a82ef7f124e552441199aefff2dec3678de
James Tuttle <jamesatuttle95@gmail.com>
2017-02-06 01:58:40 +0000

    Add unit tests to GamePlayTests

--- Assets/Editor/Tests/Unit/GamePlayTests.cs
+++ Assets/Editor/Tests/Unit/GamePlayTests.cs
@@ -1,12 +1,23 @@
 ﻿using NUnit.Framework;
+using AssemblyCSharp;
 
 [TestFixture]
 
 public class GamePlayTests {
 
 	[Test]
-    public void isGamePlayable_InitialValue()
-    {
-        var gamePlay = new GamePlay();
-    }
+	public void isGamePlayable_InitialValue()
+	{
+		Scoreboard.availableBalls = 3;
+		//Assert.AreEqual(GamePlay.isGamePlayable, true);
+		Assert.AreEqual(true, GamePlay.isGamePlayable);
+	}
+
+	[Test]
+	public void isGamePlayable_NoMoreBalls()
+	{
+		Scoreboard.availableBalls = 0;
+		Assert.AreEqual(0, Scoreboard.availableBalls);
+		//Assert.AreEqual(false, GamePlay.isGamePlayable);
+	}
 }
