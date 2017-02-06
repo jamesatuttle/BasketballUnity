b189ad4aead47b18c39e886cf4e4bf9ad20b8dbe
James Tuttle <jamesatuttle95@gmail.com>
2017-02-06 14:37:37 +0000

    Add scoreboard tests

--- Assets/Editor/Tests/Unit/ScoreboardTests.cs
+++ Assets/Editor/Tests/Unit/ScoreboardTests.cs
@@ -6,12 +6,18 @@ using UnityEngine;
 
 public class ScoreboardTests
 {
-    private Scoreboard scoreboard = new Scoreboard();
+    //private Scoreboard scoreboard = new Scoreboard();
 
     public void startScoreboard()
     {
         var scoreboard = new Scoreboard();
         scoreboard.Start();
+    }
+
+    public void startBasketDetected()
+    {
+        var basketDetected = new BasketDetected();
+        basketDetected.Start();
     }
 
     [Test]
@@ -92,5 +98,198 @@ public class ScoreboardTests
     public void showCorrectScore_onStart()
     {
         startScoreboard();
+        Assert.AreEqual("000", GameObject.Find("Score").GetComponent<TextMesh>().text);
     }
+    
+    [Test]
+    public void showCorrectScore_WhenScored()
+    {
+        Collider col = new Collider();
+        BasketDetected basketDetected = new BasketDetected();
+
+        startBasketDetected();
+        startScoreboard();
+        basketDetected.OnTriggerEnter(col);
+        basketDetected.OnTriggerExit(col);
+
+        Assert.AreEqual("010", GameObject.Find("Score").GetComponent<TextMesh>().text);
+    }
+
+    [Test]
+    public void preventScore_OnCollision_WhenNoEnterTrigger()
+    {
+        Collider col = new Collider();
+        BasketDetected basketDetected = new BasketDetected();
+
+        startBasketDetected();
+        startScoreboard();
+        basketDetected.OnTriggerExit(col);
+
+        Assert.AreNotEqual("010", GameObject.Find("Score").GetComponent<TextMesh>().text);
+        Assert.AreEqual("000", GameObject.Find("Score").GetComponent<TextMesh>().text);
+    }
+
+    [Test]
+    public void showCorrectScore_WhenScoredTwice()
+    {
+        Collider col = new Collider();
+        BasketDetected basketDetected = new BasketDetected();
+
+        startBasketDetected();
+        startScoreboard();
+        basketDetected.OnTriggerEnter(col);
+        basketDetected.OnTriggerExit(col);
+
+        basketDetected.OnTriggerEnter(col);
+        basketDetected.OnTriggerExit(col);
+
+        Assert.AreEqual("020", GameObject.Find("Score").GetComponent<TextMesh>().text);
+    }
+
+    [Test]
+    public void showCorrectScore_NotAddSingleOnHoopThrice()
+    {
+        Collider col = new Collider();
+        BasketDetected basketDetected = new BasketDetected();
+
+        startBasketDetected();
+        startScoreboard();
+        basketDetected.OnTriggerEnter(col);
+        basketDetected.OnTriggerExit(col);
+
+        basketDetected.OnTriggerEnter(col);
+        basketDetected.OnTriggerExit(col);
+
+        basketDetected.OnTriggerEnter(col);
+        basketDetected.OnTriggerExit(col);
+
+        Assert.AreNotEqual("30", GameObject.Find("Score").GetComponent<TextMesh>().text);
+    }
+
+    [Test]
+    public void showCorrectScore_OnBonus()
+    {
+        Collider col = new Collider();
+        BasketDetected basketDetected = new BasketDetected();
+
+        startBasketDetected();
+        startScoreboard();
+        basketDetected.OnTriggerEnter(col);
+        basketDetected.OnTriggerExit(col);
+
+        basketDetected.OnTriggerEnter(col);
+        basketDetected.OnTriggerExit(col);
+
+        basketDetected.OnTriggerEnter(col);
+        basketDetected.OnTriggerExit(col);
+
+        Assert.AreEqual("120", GameObject.Find("Score").GetComponent<TextMesh>().text);
+    }
+
+    [Test]
+    public void showCorrectScore_ScoreMissScoreScore()
+    {
+        Collider col = new Collider();
+        Collision collision = new Collision();
+        BasketDetected basketDetected = new BasketDetected();
+        Basketball basketball = new Basketball();
+
+        collision.gameObject.name = (GameObject.Find("Floor").GetComponent<GameObject>().name);
+
+        startBasketDetected();
+        startScoreboard();
+
+        basketDetected.OnTriggerEnter(col);
+        basketDetected.OnTriggerExit(col);
+
+        basketball.OnCollisionEnter(collision);
+
+        basketDetected.OnTriggerEnter(col);
+        basketDetected.OnTriggerExit(col);
+
+        basketDetected.OnTriggerEnter(col);
+        basketDetected.OnTriggerExit(col);
+
+        Assert.AreEqual("30", GameObject.Find("Score").GetComponent<TextMesh>().text);
+    }
+
+    [Test]
+    public void hideBonusLight_OnStart()
+    {
+        Collider col = new Collider();
+        BasketDetected basketDetected = new BasketDetected();
+
+        startBasketDetected();
+        startScoreboard();
+
+        Color bonusOff = new Color();
+        ColorUtility.TryParseHtmlString("#181717", out bonusOff);
+
+        Assert.AreEqual(bonusOff, GameObject.Find("Bonus").GetComponent<TextMesh>().color);
+    }
+
+    [Test]
+    public void hideBonusLight_AfterOneHoopScored()
+    {
+        Collider col = new Collider();
+        BasketDetected basketDetected = new BasketDetected();
+
+        startBasketDetected();
+        startScoreboard();
+
+        basketDetected.OnTriggerEnter(col);
+        basketDetected.OnTriggerExit(col);
+
+        Color bonusOff = new Color();
+        ColorUtility.TryParseHtmlString("#181717", out bonusOff);
+
+        Assert.AreEqual(bonusOff, GameObject.Find("Bonus").GetComponent<TextMesh>().color);
+    }
+
+    [Test]
+    public void hideBonusLight_AfterTwoHoopsScored()
+    {
+        Collider col = new Collider();
+        BasketDetected basketDetected = new BasketDetected();
+
+        startBasketDetected();
+        startScoreboard();
+
+        basketDetected.OnTriggerEnter(col);
+        basketDetected.OnTriggerExit(col);
+
+        basketDetected.OnTriggerEnter(col);
+        basketDetected.OnTriggerExit(col);
+
+        Color bonusOff = new Color();
+        ColorUtility.TryParseHtmlString("#181717", out bonusOff);
+
+        Assert.AreEqual(bonusOff, GameObject.Find("Bonus").GetComponent<TextMesh>().color);
+    }
+
+    [Test]
+    public void showBonusLight_OnBonus()
+    {
+        Collider col = new Collider();
+        BasketDetected basketDetected = new BasketDetected();
+
+        startBasketDetected();
+        startScoreboard();
+        basketDetected.OnTriggerEnter(col);
+        basketDetected.OnTriggerExit(col);
+
+        basketDetected.OnTriggerEnter(col);
+        basketDetected.OnTriggerExit(col);
+
+        basketDetected.OnTriggerEnter(col);
+        basketDetected.OnTriggerExit(col);
+
+        Color bonusColour = new Color();
+        ColorUtility.TryParseHtmlString("#6BD289FF", out bonusColour);
+
+        Assert.AreEqual(bonusColour, GameObject.Find("Bonus").GetComponent<TextMesh>().color);
+    }
+
+    
+
 }
