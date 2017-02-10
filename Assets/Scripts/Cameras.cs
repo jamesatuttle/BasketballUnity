0e8cd21edb85a9ca8571a0c666cc859ba7ddcf56
James Tuttle <jamesatuttle95@gmail.com>
2017-02-10 23:40:18 +0000

    Update unity file and add game play variables

--- Assets/Scripts/Cameras.cs
+++ Assets/Scripts/Cameras.cs
@@ -2,25 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Cameras : MonoBehaviour {
+public class Cameras {
 
-	Camera StartScreen;
+	static Camera StartScreen;
 	Camera FrontCamera;
 	Camera Scoreboard;
 	Camera SideHoop;
 
-	void Start()
+	public Cameras()
 	{
-
 		StartScreen = GameObject.Find ("StartScreen").GetComponent<Camera> ();
 		FrontCamera = GameObject.Find ("Front View Camera").GetComponent<Camera> ();
 		Scoreboard = GameObject.Find ("Scoreboard Camera").GetComponent<Camera> ();
 		SideHoop = GameObject.Find ("Side Hoop Camera").GetComponent<Camera> ();
 
-		StartScreen.gameObject.active = true;
+		StartScreen.gameObject.active = false;
 		FrontCamera.gameObject.active = false;
 		Scoreboard.gameObject.active = false;
 		SideHoop.gameObject.active = false;
+	}
+
+	public static void StartScreenCameraSetup()
+	{
+		StartScreen.gameObject.active = true;
 	}
 
 }
--- Assets/Scripts/GamePlay.cs
+++ Assets/Scripts/GamePlay.cs
@@ -1,14 +1,18 @@
-﻿
+﻿using AssemblyCSharp;
+
 namespace AssemblyCSharp
 {
 	public class GamePlay
 	{
 
 		public static bool isGamePlayable;
+		public bool PlayGame;
+		public bool ViewScoreboard;
+		public bool ViewLeaderboard;
 
 		public GamePlay ()
 		{
-
+			Cameras.StartScreenCameraSetup ();
 		}
 
 	}
