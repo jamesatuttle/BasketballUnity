e1a0f60d8c10e4d9abbd070fa3686821a3905d80
James Tuttle <jamesatuttle95@gmail.com>
2017-02-10 23:02:53 +0000

    Initialise Cameras

--- Assets/Scripts/Cameras.cs
+++ Assets/Scripts/Cameras.cs
@@ -4,13 +4,23 @@ using UnityEngine;
 
 public class Cameras : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
+	Camera StartScreen;
+	Camera FrontCamera;
+	Camera Scoreboard;
+	Camera SideHoop;
+
+	void Start()
+	{
+
+		StartScreen = GameObject.Find ("StartScreen").GetComponent<Camera> ();
+		FrontCamera = GameObject.Find ("Front View Camera").GetComponent<Camera> ();
+		Scoreboard = GameObject.Find ("Scoreboard Camera").GetComponent<Camera> ();
+		SideHoop = GameObject.Find ("Side Hoop Camera").GetComponent<Camera> ();
+
+		StartScreen.gameObject.active = true;
+		FrontCamera.gameObject.active = false;
+		Scoreboard.gameObject.active = false;
+		SideHoop.gameObject.active = false;
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
 }
