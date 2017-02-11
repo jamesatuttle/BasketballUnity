6c463534065d60342c881d217e372db11c908e29
James Tuttle <jamesatuttle95@gmail.com>
2017-02-11 16:48:53 +0000

    Update Camera file

--- Assets/Scripts/Cameras.cs
+++ Assets/Scripts/Cameras.cs
@@ -2,29 +2,55 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Cameras {
+public class Cameras : MonoBehaviour {
 
 	static Camera StartScreen;
-	Camera FrontCamera;
-	Camera Scoreboard;
-	Camera SideHoop;
+	static Camera FrontCamera;
+	static Camera Scoreboard;
+	static Camera SideHoop;
 
-	public Cameras()
+	public Camera camera;
+	public Camera camera2;
+
+	void Start()
 	{
-		StartScreen = GameObject.Find ("StartScreen").GetComponent<Camera> ();
-		FrontCamera = GameObject.Find ("Front View Camera").GetComponent<Camera> ();
+		Debug.Log ("Inside Camera Start");
+		//StartScreen = GameObject.Find ("StartScreen").GetComponent<Camera> ();
+		/*FrontCamera = GameObject.Find ("Front View Camera").GetComponent<Camera> ();
 		Scoreboard = GameObject.Find ("Scoreboard Camera").GetComponent<Camera> ();
-		SideHoop = GameObject.Find ("Side Hoop Camera").GetComponent<Camera> ();
+		SideHoop = GameObject.Find ("Side Hoop Camera").GetComponent<Camera> ();*/
 
-		StartScreen.gameObject.active = false;
-		FrontCamera.gameObject.active = false;
-		Scoreboard.gameObject.active = false;
-		SideHoop.gameObject.active = false;
+		/*StartScreen.gameObject.SetActive = false;
+		FrontCamera.gameObject.SetActive = false;
+		Scoreboard.gameObject.SetActive = false;
+		SideHoop.gameObject.SetActive = false;*/
+		//StartScreenCameraSetup ();
+
+		camera.enabled = true;
+		camera2.enabled = false;
+	}
+
+	void Update() {
+		//This will toggle the enabled state of the two cameras between true and false each time
+		if (Input.GetKeyDown ("backspace")) {
+			Debug.Log ("backspace");
+			camera.enabled = !camera.enabled;
+			camera2.enabled = !camera2.enabled;
+		}
 	}
 
 	public static void StartScreenCameraSetup()
 	{
-		StartScreen.gameObject.active = true;
+		Debug.Log ("Inside Camera StartScreenCameraSetup");
+
+		StartScreen.enabled = true;
+		//PlayerPrefs.SetInt("UnitySelectMonitor", 3); // Select monitor 4
+
+		/*FrontCamera.enabled = false;
+		Scoreboard.enabled = false;
+		SideHoop.enabled = false;*/
+
+		//StartScreen.gameObject.SetActive = true;
 	}
 
 }
