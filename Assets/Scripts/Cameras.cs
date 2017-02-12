824f8a58da018ef00e1b768748a36822919d1beb
James Tuttle <jamesatuttle95@gmail.com>
2017-02-12 14:49:47 +0000

    Add Camera functionality

--- Assets/Scripts/Cameras.cs
+++ Assets/Scripts/Cameras.cs
@@ -4,52 +4,25 @@ using UnityEngine;
 
 public class Cameras : MonoBehaviour {
 
-	static Camera StartScreen;
-	static Camera FrontCamera;
-	static Camera Scoreboard;
-	static Camera SideHoop;
-
-	public Camera camera;
-	public Camera camera2;
-
-	void Start()
-	{
-		Debug.Log ("Inside Camera Start");
-		//StartScreen = GameObject.Find ("StartScreen").GetComponent<Camera> ();
-		/*FrontCamera = GameObject.Find ("Front View Camera").GetComponent<Camera> ();
-		Scoreboard = GameObject.Find ("Scoreboard Camera").GetComponent<Camera> ();
-		SideHoop = GameObject.Find ("Side Hoop Camera").GetComponent<Camera> ();*/
-
-		/*StartScreen.gameObject.SetActive = false;
-		FrontCamera.gameObject.SetActive = false;
-		Scoreboard.gameObject.SetActive = false;
-		SideHoop.gameObject.SetActive = false;*/
-		//StartScreenCameraSetup ();
-
-		camera.enabled = true;
-		camera2.enabled = false;
-	}
-
-	void Update() {
-		//This will toggle the enabled state of the two cameras between true and false each time
-		if (Input.GetKeyDown ("backspace")) {
-			Debug.Log ("backspace");
-			camera.enabled = !camera.enabled;
-			camera2.enabled = !camera2.enabled;
-		}
-	}
-
 	public static void StartScreenCameraSetup()
 	{
-		Debug.Log ("Inside Camera StartScreenCameraSetup");
+		UpdateCameraPosition (0f, 2.9f, -21f, 45);
+	}
 
-		//PlayerPrefs.SetInt("UnitySelectMonitor", 3); // Select monitor 4
+	public static void MainGameCameraSetUp()
+	{
+		UpdateCameraPosition (0f, 6.5f, 5f, 34);
+	}
 
-		/*FrontCamera.enabled = false;
-		Scoreboard.enabled = false;
-		SideHoop.enabled = false;*/
+	public static void ScoreboardCameraSetUp()
+	{
+		UpdateCameraPosition (13f, 14.3f, 37.9f, 62);
+	}
 
-		//StartScreen.gameObject.SetActive = true;
+	public static void UpdateCameraPosition(float x, float y, float z, int FOV) 
+	{
+		GameObject.Find ("MainCamera").GetComponent<Camera> ().transform.position = new Vector3(x, y, z);
+		GameObject.Find ("MainCamera").GetComponent<Camera> ().GetComponent<Camera> ().fieldOfView = FOV;
 	}
 
 }
