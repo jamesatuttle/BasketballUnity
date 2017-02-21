2d6913a3b141b42df682b904e42395ffdfe4d9cd
James Tuttle <jamesatuttle95@gmail.com>
2017-02-21 14:07:04 +0000

    add functionality

--- Assets/Kinect/KinectController.cs
+++ Assets/Kinect/KinectController.cs
@@ -3,11 +3,41 @@ using System;
 
 public class KinectController : MonoBehaviour {
 
-  public Start() {
+  KinectManager manger;
 
+  Vector3 HandLeft;
+  Vector3 HandRight;
+
+  bool ballIsHeld;
+
+  public Start() {
+    manager = KinectManager.instance;
+    ballIsHeld = false;
   }
 
   public Update() {
+
+    uint userId = manager.GetPlayer1ID;
+
+    HandLeft = manager.GetRawSkeletonJointPos(userId, (int)KinectWrapper.NuiSkeletonPositionIndex.HandLeft);
+    HandRight = manager.GetRawSkeletonJointPos(userId, (int)KinectWrapper.NuiSkeletonPositionIndex.HandLeft);
+  }
+
+  public PrintHandPoints() {
+    Debug.log("HandLeft: " + HandLeft);
+    Debug.log("HandRight: " + HandRight);
+
+    float HandDifferences = HandLeft.x - HandRight.x;
+
+    float ballWidth = 0.26f; //Ball Width is 0.26 cm
+    float inch = 0.0254; //1 inch equals 2.5 cm.
+
+    if (HandDifferences > (ballWidth - inch) && HandDifferences < (ballWidth + inch) ) {
+      ballIsHeld = true;
+      while(ballIsHeld) {
+        Debug.log("Picked up ball")
+      }
+    }
 
   }
 
