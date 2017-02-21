c0071df6b50121e94f7b97828be9d8cf64f2afe0
James Tuttle <jamesatuttle95@gmail.com>
2017-02-21 23:54:25 +0000

    Add ball pick up functionality

--- Assets/Kinect/KinectController.cs
+++ Assets/Kinect/KinectController.cs
@@ -1,45 +1,118 @@
 using UnityEngine;
 using System;
+using UnityEngine.UI;
 
 public class KinectController : MonoBehaviour {
 
-  KinectManager manager = KinectManager.Instance;
+  	Vector3 HandLeft;
+  	Vector3 HandRight;
 
-  Vector3 HandLeft;
-  Vector3 HandRight;
+  	bool ballIsHeld;
 
-  bool ballIsHeld;
+	float ballWidth = 0.26f; //Ball Width is 0.26 cm
+	float inch = 0.0254f; //1 inch equals 2.5 cm.
 
-  void Start() {
-    ballIsHeld = false;
-  }
+	Vector3 callibratedPosition;
 
-  void Update() {
+	Vector3 newBallPosition;
+	 
+  	void Start() {
+		ballIsHeld = false;
 
-	uint userId = manager.GetPlayer1ID();
+		KinectManager manager = KinectManager.Instance;
 
-    HandLeft = manager.GetRawSkeletonJointPos(userId, (int)KinectWrapper.NuiSkeletonPositionIndex.HandLeft);
-    HandRight = manager.GetRawSkeletonJointPos(userId, (int)KinectWrapper.NuiSkeletonPositionIndex.HandLeft);
+		if (!manager.IsUserDetected()) {
+			Debug.Log ("User not detected");
+		}
+  	}
 
-	PrintHandPoints ();
-  }
+  	void Update() {
 
-  public void PrintHandPoints() {
-    Debug.Log("HandLeft: " + HandLeft);
-    Debug.Log("HandRight: " + HandRight);
+		KinectManager manager = KinectManager.Instance;
 
-    float HandDifferences = HandLeft.x - HandRight.x;
+		if (manager.IsUserDetected()) {
 
-    float ballWidth = 0.26f; //Ball Width is 0.26 cm
-    float inch = 0.0254f; //1 inch equals 2.5 cm.
+			uint userId = manager.GetPlayer1ID ();
 
-    if (HandDifferences > (ballWidth - inch) && HandDifferences < (ballWidth + inch) ) {
-      ballIsHeld = true;
-      while(ballIsHeld) {
-		Debug.Log ("Picked up ball");
-      }
-    }
+			HandLeft = manager.GetRawSkeletonJointPos (userId, (int)KinectWrapper.NuiSkeletonPositionIndex.HandLeft);
+			HandRight = manager.GetRawSkeletonJointPos (userId, (int)KinectWrapper.NuiSkeletonPositionIndex.HandRight);
+		
+			PrintHandPoints ();
+		}
+  	}
+		
+	public void PrintHandPoints() {
 
-  }
+		var HandDifference = -HandLeft.x - -HandRight.x;  //These x values are negative, the minus sets them positive
+
+		if (ballIsHeld) {
+			//Debug.Log ("***** Picked up ball");
+			GameObject.Find ("GestureInfo").GetComponent<Text> ().text = "Picked up ball";
+
+
+			if (HandDifference > ballWidth + (inch * 10)) {
+				//Debug.Log ("Dropped ball");
+				GameObject.Find ("GestureInfo").GetComponent<Text> ().text = "Dropped ball";
+
+				GameObject.Find ("Basketball").GetComponent<Rigidbody> ().useGravity = true; //turn gravity on
+
+				ballIsHeld = false;
+
+			} else {
+				moveBall ();
+				GameObject.Find ("Basketball").transform.position = new Vector3 (newBallPosition.x, newBallPosition.y, newBallPosition.z);
+			}
+		}
+		else {
+			//Debug.Log ("HandLeft: " + -HandLeft.x);
+			//Debug.Log ("HandRight: " + -HandRight.x);
+
+			//Debug.Log ("HandDifference: " + HandDifference + "\n");
+
+			var minimumBallWidth = ballWidth - inch;
+			var maximumBallWidth = ballWidth + inch;
+
+			bool handsInDistanceToPickUpBall = HandDifference > minimumBallWidth && HandDifference < maximumBallWidth;
+
+			if (handsInDistanceToPickUpBall) {
+				ballIsHeld = true;
+				callibrateUser ();
+			}
+		}
+			
+	}
+
+	private void callibrateUser() {
+		KinectManager manager = KinectManager.Instance;
+
+		uint userId = manager.GetPlayer1ID ();
+
+		/*Vector3 ElbowLeft = manager.GetRawSkeletonJointPos (userId, (int)KinectWrapper.NuiSkeletonPositionIndex.ElbowLeft);
+		Vector3 ElbowRight = manager.GetRawSkeletonJointPos (userId, (int)KinectWrapper.NuiSkeletonPositionIndex.ElbowRight);*/
+
+		callibratedPosition.x = (HandLeft.x + HandRight.x) / 2;
+		callibratedPosition.y = (HandLeft.y + HandRight.y) / 2;
+		callibratedPosition.z = (HandLeft.z + HandRight.z) / 2;
+
+		Debug.Log ("startBallPosition: " + callibratedPosition); 
+	}
+
+	private void moveBall() {
+
+		float HandsX = (HandLeft.x + HandRight.x) / 2;
+		float HandsY = (HandLeft.y + HandRight.y) / 2;
+		float HandsZ = (HandLeft.z + HandRight.z) / 2;
+
+		var XMovement = HandsX - callibratedPosition.x;
+		var YMovement = HandsY - callibratedPosition.y;
+		var ZMovement = HandsZ - callibratedPosition.z;
+
+		newBallPosition.x = Basketball.InitialBallPosition.x + XMovement;
+		newBallPosition.y = Basketball.InitialBallPosition.y + YMovement;
+		newBallPosition.z = Basketball.InitialBallPosition.z + ZMovement;
+
+		GameObject.Find ("Basketball").GetComponent<Rigidbody> ().constraints = RigidbodyConstraints.None;
+
+	}
 
 }
