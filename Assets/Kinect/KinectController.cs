d9c3eff307ebc464c045fb8e5be378bec11d0d89
James Tuttle <jamesatuttle95@gmail.com>
2017-02-22 11:25:52 +0000

    Refactor kinect controller

--- Assets/Kinect/KinectController.cs
+++ Assets/Kinect/KinectController.cs
@@ -2,69 +2,63 @@ using UnityEngine;
 using System;
 using UnityEngine.UI;
 
-public class KinectController : MonoBehaviour {
+public class KinectController : MonoBehaviour
+{
 
-  	Vector3 HandLeft;
-  	Vector3 HandRight;
+	Vector3 HandLeft;
+	Vector3 HandRight;
 
-  	bool ballIsHeld;
+	bool ballIsHeld;
 
 	float ballWidth = 0.26f; //Ball Width is 0.26 cm
 	float inch = 0.0254f; //1 inch equals 2.5 cm.
 
 	Vector3 callibratedPosition;
 
-	Vector3 newBallPosition;
-	 
-  	void Start() {
+	void Start ()
+	{
 		ballIsHeld = false;
-  	}
+	}
 
-  	void Update() {
+	void Update ()
+	{ 
 		try {
-		KinectManager manager = KinectManager.Instance;
+			KinectManager manager = KinectManager.Instance;
 
-		if (manager.IsUserDetected() && GamePlay.GameIsPlayable) {
+			if (manager.IsUserDetected () && GamePlay.GameIsPlayable) {
 
-			uint userId = manager.GetPlayer1ID ();
+				uint userId = manager.GetPlayer1ID ();
 
-			HandLeft = manager.GetRawSkeletonJointPos (userId, (int)KinectWrapper.NuiSkeletonPositionIndex.HandLeft);
-			HandRight = manager.GetRawSkeletonJointPos (userId, (int)KinectWrapper.NuiSkeletonPositionIndex.HandRight);
-		
-			PrintHandPoints ();
-		}
+				HandLeft = manager.GetRawSkeletonJointPos (userId, (int)KinectWrapper.NuiSkeletonPositionIndex.HandLeft);
+				HandRight = manager.GetRawSkeletonJointPos (userId, (int)KinectWrapper.NuiSkeletonPositionIndex.HandRight);
+
+				PrintHandPoints ();
+			}
 		} catch (Exception e) {
-			Debug.Log ("An error occured");
+			Debug.Log ("An error occured: " + e.Message);
 		}
-  	}
-		
-	public void PrintHandPoints() {
+	}
+
+	public void PrintHandPoints ()
+	{
 
 		var HandDifference = -HandLeft.x - -HandRight.x;  //These x values are negative, the minus sets them positive
 
 		if (ballIsHeld) {
-			//Debug.Log ("***** Picked up ball");
 			GameObject.Find ("GestureInfo").GetComponent<Text> ().text = "Picked up ball";
 
+			if (HandDifference > ballWidth + (inch * 10)) { //check they haven't dropped the ball
 
-			if (HandDifference > ballWidth + (inch * 10)) {
-				//Debug.Log ("Dropped ball");
 				GameObject.Find ("GestureInfo").GetComponent<Text> ().text = "Dropped ball";
 
-				GameObject.Find ("Basketball").GetComponent<Rigidbody> ().useGravity = true; //turn gravity on
+				Basketball.setBallGravity (true); //turn gravity on
 
 				ballIsHeld = false;
 
 			} else {
 				moveBall ();
-				GameObject.Find ("Basketball").transform.position = new Vector3 (newBallPosition.x, newBallPosition.y, newBallPosition.z);
 			}
-		}
-		else {
-			//Debug.Log ("HandLeft: " + -HandLeft.x);
-			//Debug.Log ("HandRight: " + -HandRight.x);
-
-			//Debug.Log ("HandDifference: " + HandDifference + "\n");
+		} else {
 
 			var minimumBallWidth = ballWidth - inch;
 			var maximumBallWidth = ballWidth + inch;
@@ -76,25 +70,25 @@ public class KinectController : MonoBehaviour {
 				callibrateUser ();
 			}
 		}
-			
+
 	}
 
-	private void callibrateUser() {
+	private void callibrateUser ()
+	{
 		KinectManager manager = KinectManager.Instance;
 
 		uint userId = manager.GetPlayer1ID ();
-
-		/*Vector3 ElbowLeft = manager.GetRawSkeletonJointPos (userId, (int)KinectWrapper.NuiSkeletonPositionIndex.ElbowLeft);
-		Vector3 ElbowRight = manager.GetRawSkeletonJointPos (userId, (int)KinectWrapper.NuiSkeletonPositionIndex.ElbowRight);*/
 
 		callibratedPosition.x = (HandLeft.x + HandRight.x) / 2;
 		callibratedPosition.y = (HandLeft.y + HandRight.y) / 2;
 		callibratedPosition.z = (HandLeft.z + HandRight.z) / 2;
 
-		Debug.Log ("startBallPosition: " + callibratedPosition); 
 	}
 
-	private void moveBall() {
+	private void moveBall ()
+	{
+
+		Vector3 newBallPosition;
 
 		float HandsX = (HandLeft.x + HandRight.x) / 2;
 		float HandsY = (HandLeft.y + HandRight.y) / 2;
@@ -109,6 +103,8 @@ public class KinectController : MonoBehaviour {
 		newBallPosition.z = Basketball.InitialBallPosition.z + ZMovement;
 
 		GameObject.Find ("Basketball").GetComponent<Rigidbody> ().constraints = RigidbodyConstraints.None;
+
+		GameObject.Find ("Basketball").transform.position = new Vector3 (newBallPosition.x, newBallPosition.y, newBallPosition.z);
 
 	}
 
