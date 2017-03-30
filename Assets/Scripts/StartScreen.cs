d66f04e3c3836d2e282107e922da1041debd02cb
James Tuttle <jamesatuttle95@gmail.com>
2017-03-30 23:04:27 +0100

    refactor start screen script

--- Assets/Scripts/StartScreen.cs
+++ Assets/Scripts/StartScreen.cs
@@ -5,159 +5,145 @@ using UnityEngine.UI;
 
 public class StartScreen : MonoBehaviour {
 
-	static Text StartGameText;
-	static Text ViewScoreboardText;
-	static Text ViewLeaderboardText;
-	static Text HowToPlayText;
+	public static StartScreen instance;
 
-	bool StartGame;
-	bool ViewScoreboard;
-	bool ViewLeaderboard;
-	bool HowToPlay;
+	Text StartGame;
+	Text ViewScoreboard;
+	Text ViewLeaderboard;
+	Text HowToPlayText;
+	Text LoginText;
+	Text LeaderboardTitle;
+	Text Leaderboard;
 
-	void Awake()
-	{
-		GamePlay.ViewingStartScreen = true;
-		StartGameText = GameObject.Find ("Start Game").GetComponent<Text> ();
-		ViewScoreboardText = GameObject.Find ("View Scoreboard").GetComponent<Text> ();
-		ViewLeaderboardText = GameObject.Find ("View Leaderboard").GetComponent<Text> ();
-		HowToPlayText = GameObject.Find ("How to play").GetComponent<Text> ();
+	public InputField FirstName;
+	public InputField LastName;
 
-		StartGame = false;
-		ViewScoreboard = false;
-		ViewLeaderboard = false;
-		HowToPlay = false;
+	Button NextButton;
+	Button BackButton;
+
+	void Awake() {
+		instance = this;
 		GamePlay.GameIsPlayable = false;
+		GamePlay.ActiveScreenValue = (int)GamePlay.ActiveScreen.startScreen;
+
+		StartGame = GameObject.Find ("Start Game").GetComponent<Text> ();
+		ViewScoreboard = GameObject.Find ("View Scoreboard").GetComponent<Text> ();
+		ViewLeaderboard = GameObject.Find ("View Leaderboard").GetComponent<Text> ();
+		HowToPlayText = GameObject.Find ("How to play").GetComponent<Text> ();
+		LoginText = GameObject.Find ("LoginText").GetComponent<Text> ();
+		FirstName = GameObject.Find ("FirstNameInput").GetComponent<InputField> ();
+		LastName = GameObject.Find ("LastNameInput").GetComponent<InputField> ();
+		NextButton = GameObject.Find ("NextButton").GetComponent<Button> ();
+		BackButton = GameObject.Find ("BackButton").GetComponent<Button> ();
+		LeaderboardTitle = GameObject.Find ("Leaderboard Title").GetComponent<Text> ();
+		Leaderboard = GameObject.Find ("Leaderboard").GetComponent<Text> ();
 	}
 
 	void Start () {
-		//if (GamePlay.ViewingStartScreen)
-			SetUpStartScreen ();
-		//else 
-		//	ClearStartScreen ();
+		SetUpStartScreen ();
 	}
 		
 	// Update is called once per frame
 	void Update () {
-		if (GamePlay.ViewingStartScreen) {
-			if (Input.GetKeyDown ("down") && GamePlay.ViewingStartScreen) {
-				if (StartGameText.color == Color.yellow)
-					ScoreboardTextActive ();
-				else if (ViewScoreboardText.color == Color.yellow)
-					LeaderboardTextActive ();
-				else if (ViewLeaderboardText.color == Color.yellow)
-					HowToPlayTextActive ();
-				else if (HowToPlayText.color == Color.yellow)
-					StartGameTextActive ();
+		if (GamePlay.ActiveScreenValue == (int)GamePlay.ActiveScreen.startScreen) {
+			if (Input.GetKeyDown(KeyCode.DownArrow)) {
+				if (OptionSelected(StartGame))
+					SetTextActive(ViewScoreboard);
+				else if (OptionSelected(ViewScoreboard))
+					SetTextActive(ViewLeaderboard);
+				else if (OptionSelected(ViewLeaderboard))
+					SetTextActive(HowToPlayText);
+				else if (OptionSelected(HowToPlayText))
+					SetTextActive(StartGame);
 			}
 
-			if (Input.GetKeyDown ("return")) {
-				Debug.Log ("return key was pressed");
+			if (Input.GetKeyDown (KeyCode.Return)) {
+				//Debug.Log ("return key was pressed");
 
-				if (StartGame) {
-					Login.SetupLogin ();//GamePlay.LoginStarted = true;
-					GamePlay.ViewingStartScreen = false;
-				//GamePlay.SetUpPregame (); //SetUpGame (); 
-				}
-				else if (ViewScoreboard)
-					SetUpScoreboardView ();
-				else if (ViewLeaderboard) {
-
-				}
+				if (OptionSelected(StartGame))
+					GamePlay.ActiveScreenValue = (int)GamePlay.ActiveScreen.enterName;
+				else if (OptionSelected(ViewScoreboard))
+					GamePlay.ActiveScreenValue = (int)GamePlay.ActiveScreen.scoreboard;
+				else if (OptionSelected(ViewLeaderboard))
+					GamePlay.ActiveScreenValue = (int)GamePlay.ActiveScreen.leaderboard;
+				else if (OptionSelected(HowToPlayText))
+					GamePlay.ActiveScreenValue = (int)GamePlay.ActiveScreen.howToPlay;
 			}
 		} else {
 			ClearStartScreen ();
 		}
 	}
 
-	public void SetUpStartScreen()
-	{
-		Cameras.StartScreenCameraSetup ();
-		GameObject.Find ("Start Game").GetComponent<Text> ().text = "START GAME";
-		GameObject.Find ("View Scoreboard").GetComponent<Text> ().text = "SCOREBOARD";
-		GameObject.Find ("View Leaderboard").GetComponent<Text> ().text = "LEADERBOARD";
-		GameObject.Find ("How to play").GetComponent<Text> ().text = "HOW TO PLAY";
+	void HideButton(Button button) {
+		button.image.enabled = false;
+		button.GetComponentInChildren<Text> ().text = "";
+	}
+
+	void HideInputField(InputField inputField) {
+		inputField.image.enabled = false;
+		inputField.placeholder.enabled = false;
+		inputField.text = "";
+	}
+
+	void HideText(Text text) {
+		text.text = "";
+	}
+
+	bool OptionSelected(Text text) {
+		if (text.color == Color.yellow)
+			return true;
+		else
+			return false;
+	}
+
+	public void SetUpStartScreen() {
+		GamePlay.ActiveScreenValue = (int)GamePlay.ActiveScreen.startScreen;
+		StartGame.text = "START GAME";
+		ViewScoreboard.text = "SCOREBOARD";
+		ViewLeaderboard.text = "LEADERBOARD";
+		HowToPlayText.text = "HOW TO PLAY";
+
+		HideInputField (FirstName);
+		HideInputField (LastName);
+		HideButton (NextButton);
+		HideButton (BackButton);
+		HideText (LoginText);
+		HideText (LeaderboardTitle);
+		HideText (Leaderboard);
 
 		//GameObject.Find ("spotlights").active = false;
-		GameObject.Find ("Leaderboard Title").GetComponent<Text> ().text = "";
-		GameObject.Find ("Leaderboard").GetComponent<Text> ().text = "";
 
 		Basketball.UpdateFixedBasketballPosition(0f, 1.53f, -15.44f);
-		StartGameTextActive ();
+		SetTextActive (StartGame);
 	}
 
-	public static void ClearStartScreen()
-	{
-		StartGameText.text = "";
-		ViewScoreboardText.text = "";
-		ViewLeaderboardText.text = "";
-		HowToPlayText.text = "";
+	public void ClearStartScreen() {
+		HideText (StartGame);
+		HideText (ViewLeaderboard);
+		HideText (ViewScoreboard); 
+		HideText (HowToPlayText);
 	}
 
-	public void StartGameTextActive()
-	{
-		StartGameText.color = Color.yellow;
-		ViewScoreboardText.color = Color.white;
-		ViewLeaderboardText.color = Color.white;
+	private void SetTextActive(Text text) {
+
+		StartGame.color = Color.white;
+		ViewScoreboard.color = Color.white;
+		ViewLeaderboard.color = Color.white;
 		HowToPlayText.color = Color.white;
 
-		StartGame = true;
-		ViewScoreboard = false;
-		ViewLeaderboard = false;
-		HowToPlay = false;
-	}
-
-	public void ScoreboardTextActive()
-	{
-		StartGameText.color = Color.white;
-		ViewScoreboardText.color = Color.yellow;
-		ViewLeaderboardText.color = Color.white;
-		HowToPlayText.color = Color.white;
-
-		StartGame = false;
-		ViewScoreboard = true;
-		ViewLeaderboard = false;
-		HowToPlay = false;
-	}
-
-	public void LeaderboardTextActive()
-	{
-		StartGameText.color = Color.white;
-		ViewScoreboardText.color = Color.white;
-		ViewLeaderboardText.color = Color.yellow;
-		HowToPlayText.color = Color.white;
-
-		StartGame = false;
-		ViewScoreboard = false;
-		ViewLeaderboard = true;
-		HowToPlay = false;
-	}
-
-	public void HowToPlayTextActive()
-	{
-		StartGameText.color = Color.white;
-		ViewScoreboardText.color = Color.white;
-		ViewLeaderboardText.color = Color.white;
-		HowToPlayText.color = Color.yellow;
-
-		StartGame = false;
-		ViewScoreboard = false;
-		ViewLeaderboard = false;
-		HowToPlay = true;
-	}
-
-	/*public void SetUpGame()
-	{
-		Cameras.MainGameCameraSetUp ();
-		Basketball.ResetBall ();
-		ClearStartScreen ();
-		HUD.countdown = true;
-		//GamePlay.PlayGame = true;
-	}*/
-
-	public void SetUpScoreboardView()
-	{
-		ClearStartScreen ();
-		Cameras.ScoreboardCameraSetUp ();
+		switch (text.name) {
+		case "Start Game":
+			text.color = Color.yellow;
+			break;
+		case "View Scoreboard":
+			text.color = Color.yellow;
+			break;
+		case "View Leaderboard":
+			text.color = Color.yellow;
+			break;
+		case "How to play":
+			text.color = Color.yellow;
+			break;
+		}
 	}
 }
