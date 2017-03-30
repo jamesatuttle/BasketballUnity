925566e565e07702b24d186688d942b019c5c122
James Tuttle <jamesatuttle95@gmail.com>
2017-03-30 23:05:05 +0100

    add initial login screen

--- Assets/Scripts/Login.cs
+++ Assets/Scripts/Login.cs
@@ -5,93 +5,108 @@ using UnityEngine.UI;
 
 public class Login : MonoBehaviour {
 
-	private static Text LoginText;
-	private static Text Yes;
-	private static Text No;
-	private static Text _firstNameLabel;
-	private static Text _lastNameLabel;
-	private static bool _registering;
+	public static Login instance;
 
-	public static InputField FirstName;
+	public Text LoginText;
+	private Text Yes;
+	private Text No;
+	private Text _firstNameLabel;
+	private Text _lastNameLabel;
+	private bool _registering;
+
+	private InputField FirstName;
+	private InputField LastName;
+
+	Button NextButton;
+	Button BackButton;
+
+	StartScreen startScreen;
 
 	private string _firstName;
+
+	void Awake() {
+		instance = this;
+	}
 
 	// Use this for initialization
 	void Start () {
 		LoginText = GameObject.Find ("LoginText").GetComponent<Text> ();
 		Yes = GameObject.Find ("Yes").GetComponent<Text> ();
 		No = GameObject.Find ("No").GetComponent<Text> ();
-		_firstNameLabel = GameObject.Find ("FirstNameLabel").GetComponent<Text> ();
-		_lastNameLabel = GameObject.Find ("LastNameLabel").GetComponent<Text> ();
 
-		FirstName = GameObject.Find ("FirstName").GetComponent<InputField> ();
+		FirstName = GameObject.Find ("FirstNameInput").GetComponent<InputField> ();
+		LastName = GameObject.Find ("LastNameInput").GetComponent<InputField> ();
 
-		LoginText.text = "";
-		Yes.text = "";
-		No.text = "";
-		_firstNameLabel.text = "";
-		_lastNameLabel.text = "";
+		NextButton = GameObject.Find ("NextButton").GetComponent<Button> ();
+		BackButton = GameObject.Find ("BackButton").GetComponent<Button> ();
+
+		HideText (LoginText);
+		HideText (Yes);
+		HideText (No);
 
 		_registering = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (GamePlay.ViewingLoginInitial)
-			LoginYesNoController ();
-
-		if (_registering) {
-			GamePlay.ViewingLoginInitial = false;
-			Yes.text = "";
-			No.text = "";
-			Registering ();
+		if (GamePlay.ActiveScreenValue == (int)GamePlay.ActiveScreen.enterName) {
+			SetupLogin ();
 		}
 	}
 
-	public static void SetupLogin() {
-		GamePlay.ViewingLoginInitial = true;
-		//StartScreen.ClearStartScreen ();
-		LoginText.text = "HAVE YOU PLAYED BEFORE?";
-		Yes.text = "YES";
-		No.text = "NO";
-		HighlightText (No);
-
-
+	void HideButton(Button button) {
+		button.image.enabled = false;
+		button.GetComponentInChildren<Text> ().text = "";
 	}
 
-	public static void LoginYesNoController() {
-		//print ("inside controller: " + GamePlay.ViewingLoginInitial);
+	void ShowButton(Button button) {
+		button.image.enabled = true;
 
-		if (Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.LeftArrow)) {
-			if (IsHighlighted (Yes)) {  
-				HighlightText (No);
-			} else {
-				HighlightText (Yes);
-			}
-		}
+		if (button.name == "NextButton")
+			button.GetComponentInChildren<Text> ().text = "NEXT";
 
-		if (Input.GetKeyDown (KeyCode.Return)) {
-			if (IsHighlighted (Yes)) {
-				print ("pressed yes");
-			} else {
-				print ("pressed nah");
-				_registering = true;
-			}
-		}
+		if (button.name == "BackButton")
+			button.GetComponentInChildren<Text> ().text = "BACK";
 	}
 
-	public static void Registering() {
+	void HideInputField(InputField inputField) {
+		inputField.image.enabled = false;
+		inputField.placeholder.enabled = false;
+	}
+
+	public void HideAskNameScreen() {
+		HideButton (NextButton);
+		HideButton (BackButton);
+		HideInputField (FirstName);
+		HideInputField (LastName);
+		HideText (LoginText);
+	}
+
+	void ShowInputField(InputField inputField) {
+		inputField.image.enabled = true;
+		inputField.ActivateInputField ();
+	}
+
+	void HideText(Text text) {
+		text.text = "";
+	}
+
+	public void BackButton_Pressed() {
+		GamePlay.ActiveScreenValue = (int)GamePlay.ActiveScreen.startScreen;
+		StartScreen.instance.SetUpStartScreen ();
+	}
+
+	public void SetupLogin() {
 		LoginText.text = "PLEASE ENTER YOUR NAME";
-		_firstNameLabel.text = "FIRST NAME";
-		_lastNameLabel.text = "LAST NAME";
 		HighlightText (LoginText);
 
-		FirstName.ActivateInputField ();
+		ShowInputField (FirstName);
+		ShowInputField (LastName);
 
-		if (Input.GetKeyDown (KeyCode.A)) {
+		ShowButton (NextButton);
+		ShowButton (BackButton);
 
-		}
-			
+		BackButton.onClick.AddListener (BackButton_Pressed);
 	}
 
 	public void FirstNameTextChanged(string firstName) {
@@ -99,7 +114,7 @@ public class Login : MonoBehaviour {
 		print (_firstName);
 	}
 
-	public static void HighlightText(Text text) {
+	public  void HighlightText(Text text) {
 		text.color = Color.yellow;
 
 		if (text == Yes)
@@ -108,7 +123,7 @@ public class Login : MonoBehaviour {
 			Yes.color = Color.white;
 	}
 
-	public static bool IsHighlighted(Text text) {
+	public  bool IsHighlighted(Text text) {
 		if (text.color == Color.yellow)
 			return true;
 		else
