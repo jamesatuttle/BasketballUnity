a33f46ae8159e9c41e04f2b608f60df2bc6f37c4
James Tuttle <jamesatuttle95@gmail.com>
2017-04-19 19:59:29 +0100

    add comments to BasketDetected.cs

--- Assets/Scripts/BasketDetected.cs
+++ Assets/Scripts/BasketDetected.cs
@@ -2,45 +2,55 @@
 
 public class BasketDetected : MonoBehaviour {
 
-	public static int basketCount;
-	private bool scored = false;
-	private int bonusSetting = 2;
+	public static int basketCount; //a public integer variable to track the number of repeated baskets
+	private bool scored = false; //booean variable to store when a hoop is scored
+	private int bonusSetting = 2; //integer variable to store the bonus hoop score threshold
 
-    public void Start () {
+	/*
+	* Start is called when the game loads
+	* This initialises the repeat basket count to 0
+	*/
+  public void Start () {
 		basketCount = 0;
-    }
+  }
 
+	/*
+	* Update is called approximately 20 frames per second
+	* If the user has scored then update the scoreboard as appropriate
+	*/
 	void Update () {
-		if (scored && GamePlay.ActiveScreenValue == (int)GamePlay.ActiveScreen.mainGame)
+		if (scored && GamePlay.ActiveScreenValue == (int)GamePlay.ActiveScreen.mainGame) //if the scored is true and the main game is being played...
 		{
-			scored = false;
+			scored = false; //set scored to false to prevent looping
 
-			if (basketCount < bonusSetting) {
+			if (basketCount < bonusSetting) { //if the current basket count is less than the bonus threshold...
 
 				Scoreboard.AddToScore(10);
 
 				Scoreboard.LightUpScoreboardBonus (false); // set the bonus text to black
 
-				basketCount++;
+				basketCount++; //add one to the basket count
 
-			} else if (basketCount == bonusSetting) {
+			} else if (basketCount == bonusSetting) { //if the basket count is equal to the bonus setting...
 
 				Scoreboard.AddToScore(100);
 
 				Scoreboard.LightUpScoreboardBonus (true); // set the bonus text to green
 
-				basketCount = 0;
+				basketCount = 0; //clear the basket count - the bonus count restarts
 			}
 
 			Basketball.instance.ResetBall ();
 		}
 	}
 
-    public void OnTriggerEnter (Collider col)
+	/*
+	* Check if the user has scored a hoop by checking if the basketball has passed through the hoop trigger
+	*/
+  public void OnTriggerEnter (Collider col)
 	{
 		if (col.gameObject.name == "Basketball") {
-			print ("triggered score");
-			scored = true;
+			scored = true; //set scored to true so that the score can be updated
 		}
 	}
 
