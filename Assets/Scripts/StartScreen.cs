5b5d81a40855b6978a1687e521f48b1f1df3fc34
James Tuttle <jamesatuttle95@gmail.com>
2017-04-20 02:00:59 +0100

    add comments to start screen

--- Assets/Scripts/StartScreen.cs
+++ Assets/Scripts/StartScreen.cs
@@ -6,8 +6,9 @@ using UnityEngine.UI;
 
 public class StartScreen : MonoBehaviour {
 
-	public static StartScreen instance;
+	public static StartScreen instance; //create an instance of the class, so that other classes can access instances of its methods
 
+	//the Text objects in the game environment
 	Text StartGame;
 	Text ViewScoreboard;
 	Text ViewLeaderboard;
@@ -18,19 +19,22 @@ public class StartScreen : MonoBehaviour {
 	Text Leaderboard_Usernames;
 	Text Leaderboard_Scores;
 
+	//the text object which would display if the kinect is not connected when the game starts
 	private Text _kinectConnectedText;
 
 	InputField UserName;
 
+	//the buttons from the start screen
 	Button NextButton;
 	Button BackButton;
 	Button YesButton;
 	Button NoButton;
 
+	//Awake is called at the start of the game, used to initialise variables
 	void Awake() {
 		instance = this;
 		GamePlay.GameIsPlayable = false;
-		GamePlay.ActiveScreenValue = (int)GamePlay.ActiveScreen.startScreen;
+		GamePlay.ActiveScreenValue = (int)GamePlay.ActiveScreen.startScreen; //set the start screen to display when the game loads
 
 		StartGame = GameObject.Find ("Start Game").GetComponent<Text> ();
 		ViewScoreboard = GameObject.Find ("View Scoreboard").GetComponent<Text> ();
@@ -49,12 +53,18 @@ public class StartScreen : MonoBehaviour {
 		_kinectConnectedText = GameObject.Find ("KinectConnected").GetComponent<Text> ();
 	}
 
+	//called after Awake, at the start of the game
 	void Start () {
 		GamePlay.ActiveScreenValue = (int)GamePlay.ActiveScreen.startScreen;
-		SetTextActive (StartGame);
+		SetTextActive (StartGame); //sets the START GAME text to hightlight at the start
 	}
-		
-	// Update is called once per frame
+
+	/*
+	* Update is called once per frame
+	* controls the funcitonality of the start screen
+	* if the user presses the up and down keys on the keyboard then they can move through the start screen options
+	* if the user presses the enter key on an option, the options corresponding screen is displayed
+	*/
 	void Update () {
 
 		try {
@@ -94,7 +104,7 @@ public class StartScreen : MonoBehaviour {
 							GamePlay.ActiveScreenValue = (int)GamePlay.ActiveScreen.playedBeforeQuestion;
 							_kinectConnectedText.text = "";
 						}
-						
+
 					} else if (OptionSelected(ViewScoreboard))
 						GamePlay.ActiveScreenValue = (int)GamePlay.ActiveScreen.scoreboard;
 					else if (OptionSelected(ViewLeaderboard))
@@ -111,21 +121,37 @@ public class StartScreen : MonoBehaviour {
 		}
 	}
 
+	/*
+	* A generic method to hide the button which is passed in
+	* the button image is disabled and the text is cleared
+	*/
 	void HideButton(Button button) {
 		button.image.enabled = false;
 		button.GetComponentInChildren<Text> ().text = "";
 	}
 
+	/*
+	* A generic method to hide the input field which is passed in
+	* the input field image is disabled
+	* and the placeholder text and input text is cleared
+	*/
 	void HideInputField(InputField inputField) {
 		inputField.image.enabled = false;
 		inputField.placeholder.enabled = false;
 		inputField.text = "";
 	}
 
+	/*
+	* A generic method to hide the text which is passed in
+	*/
 	void HideText(Text text) {
 		text.text = "";
 	}
 
+	/*
+	* Returns true if the text passed in is highlighted in yellow,
+	* otherwise returns false
+	*/
 	bool OptionSelected(Text text) {
 		if (text.color == Color.yellow)
 			return true;
@@ -133,6 +159,10 @@ public class StartScreen : MonoBehaviour {
 			return false;
 	}
 
+	/*
+	* Sets up the start screen by displaying the start screen options
+	* hiding the inputField, buttons, and non-required text
+	*/
 	public void SetUpStartScreen() {
 		GamePlay.ActiveScreenValue = (int)GamePlay.ActiveScreen.startScreen;
 		StartGame.text = "START GAME";
@@ -151,18 +181,24 @@ public class StartScreen : MonoBehaviour {
 		HideText (Leaderboard_Usernames);
 		HideText (Leaderboard_Scores);
 
-		Scoreboard.instance.ResetScore ();
+		Scoreboard.instance.ResetScore (); //resets the score back to 0
 
-		Basketball.instance.UpdateFixedBasketballPosition(0f, 1.53f, -15.44f);
+		Basketball.instance.UpdateFixedBasketballPosition(0f, 1.53f, -15.44f); //sets the basketball positon for the start screen
 	}
 
+	/*
+	* A method to clear the start screen by hiding the text options
+	*/
 	public void ClearStartScreen() {
 		HideText (StartGame);
 		HideText (ViewLeaderboard);
-		HideText (ViewScoreboard); 
+		HideText (ViewScoreboard);
 		HideText (HowToPlayText);
 	}
 
+	/*
+	* A method to highlight the passed in text in yellow and set the other options to white
+	*/
 	private void SetTextActive(Text text) {
 		StartGame.color = Color.white;
 		ViewScoreboard.color = Color.white;
